38cb77e87f67319c532389fda16b60df9e1451a7
Aji Purnomo <semlakune@gmail.com>
2022-10-07 16:45:33 +0700

    customer

--- customer/App.js
+++ customer/App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, TextInput, ScrollView} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import {useState} from "react";
 
 export default function App() {
+  const [registerForm, setRegisterForm] = useState({
+    name: '',
+    email: "",
+    password: "",
+    phoneNumber: "",
+    address: ""
+  })
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.form}>
+        <Text>Full Name</Text>
+        <TextInput style={styles.input} onChangeText={setRegisterForm.name} value={registerForm.name}  />
+
+        <Text>Email Address</Text>
+        <TextInput style={styles.input} onChangeText={setRegisterForm.email} value={registerForm.email}  />
+
+        <Text>Password</Text>
+        <TextInput style={styles.input} onChangeText={setRegisterForm.password} value={registerForm.password}  />
+
+        <Text>Phone Number</Text>
+        <TextInput style={styles.input} onChangeText={setRegisterForm.phoneNumber} value={registerForm.phoneNumber}  />
+
+        <Text>Address</Text>
+        <TextInput style={styles.input} onChangeText={setRegisterForm.address} value={registerForm.address}  />
+
+        <StatusBar style="auto" />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +42,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  form: {
+    flex: 1,
+    width: '90%',
+    height: '100%',
+    marginTop: 20
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  }
 });
