149e1f1fc3c57974871920cebe92a7f2a1a7c623
Aji Purnomo <semlakune@gmail.com>
2022-10-07 17:52:14 +0700

    tab navigation and login - driver app

--- driver/src/screens/ChatScreen.js
+++ driver/src/screens/ChatScreen.js
@@ -1,11 +1,11 @@
 import {Text, View, StyleSheet} from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function HomeScreen() {
+export default function ChatScreen() {
     return (
         <SafeAreaView style={styles.container}>
             <View>
-                <Text>Home</Text>
+                <Text>Chat</Text>
             </View>
         </SafeAreaView>
     )
