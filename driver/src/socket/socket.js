0d640ffcd45eb0a2cd17b2cb961ba9401ca31df8
isan <ihellostereo@gmail.com>
2022-10-13 00:48:54 +0700

    Fixing: minor fix on base url config

--- driver/src/socket/socket.js
+++ driver/src/socket/socket.js
@@ -1,2 +1,3 @@
 import io from 'socket.io-client'
-export const socketInstance = io('https://fa16-2001-448a-2040-44a9-a4e0-bc1-6332-2593.ap.ngrok.io') // ganti ke ip local/ link deploy
+import baseUrl from '../constants/baseUrl'
+export const socketInstance = io(baseUrl) // ganti ke ip local/ link deploy
