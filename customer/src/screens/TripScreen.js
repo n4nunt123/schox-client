02f6e91206902e9848be6a97e1c51ee8060a6bad
Aji Purnomo <semlakune@gmail.com>
2022-10-12 23:39:51 +0700

    on going feature maps direction 2

--- customer/src/screens/TripScreen.js
+++ customer/src/screens/TripScreen.js
@@ -1,18 +1,71 @@
-import { useState } from "react";
-import { View, Text } from "react-native";
-import { socketInstance } from '../socket/socket'
+import { StyleSheet, Text, View } from "react-native";
+import { socketInstance } from "../socket/socket";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import * as Location from "expo-location";
+import { useState, useEffect } from "react";
+import { useFocusEffect } from "@react-navigation/native";
+import MapViewDirections from "react-native-maps-directions";
+import * as React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
+const mapRef = React.createRef();
 export default function TripScreen() {
-  const [driverCoordinate, setDriverCoordinate] = useState({})
+    const [driverCoordinate, setDriverCoordinate] = useState({});
+    const [myLocation, setMyLocation] = useState({});
 
-  socketInstance.on("recieve:interval", (data) => {
-    setDriverCoordinate(data)
-  })
+    socketInstance.on("recieve:interval", (data) => {
+        setDriverCoordinate(data);
+    });
 
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>TRIP SCREEN</Text>
-      <Text>{JSON.stringify(driverCoordinate)}</Text>
-    </View>
-  )
+    const getLocation = async () => {
+        try {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                return;
+            }
+            let location = await Location.getCurrentPositionAsync({
+                accuracy: Location.Accuracy.Balanced,
+                enableHighAccuracy: true,
+                timeInterval: 5,
+            });
+            await mapRef.current.animateCamera({
+                center: {
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude,
+                },
+            });
+
+            setMyLocation({
+                longitude: location.coords.longitude,
+                latitude: location.coords.latitude,
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    useEffect(() => {
+      getLocation();
+    }, [])
+    console.log(myLocation);
+    return (
+        <View style={styles.container}>
+            <Text>TRIP SCREEN</Text>
+            {/* <Text>{JSON.stringify(driverCoordinate)}</Text> */}
+            
+        </View>
+    );
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#DEE9FF",
+    },
+    map: {
+        height: 500,
+        width: "95%",
+        marginTop: 80
+    },
+});
--- customer/src/socket/socket.js
+++ customer/src/socket/socket.js
@@ -1,2 +1,2 @@
 import io from 'socket.io-client'
-export const socketInstance = io('https://f635-2001-448a-2040-44a9-1d9d-ce8e-5dad-1839.ap.ngrok.io') // ganti ke ip local/ link deploy
+export const socketInstance = io('https://fa16-2001-448a-2040-44a9-a4e0-bc1-6332-2593.ap.ngrok.io') // ganti ke ip local/ link deploy
